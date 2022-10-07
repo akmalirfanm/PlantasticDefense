6ce075604943c58e50a9ff7f09405787e9a77e25
Lesmannn <lesmanawahyu25@gmail.com>
2022-10-07 11:24:57 +0700

    Add Audio Testing

--- Assets/Scripts/Module_GameAudio/AudioManager.cs
+++ Assets/Scripts/Module_GameAudio/AudioManager.cs
@@ -22,11 +22,35 @@ namespace Plantastic.Module_GameAudio
         private AudioClip _buyTower;
         [SerializeField]
         private AudioClip _sellTower;
+        [SerializeField]
+        private AudioClip _plantAttack1;
+        [SerializeField]
+        private AudioClip _plantAttack2;
+        [SerializeField]
+        private AudioClip _plantAttack3;
+        [SerializeField]
+        private AudioClip _plantAttack4;
+        [SerializeField]
+        private AudioClip _upgradeTower;
+        [SerializeField]
+        private AudioClip _boss;
+        [SerializeField]
+        private AudioClip _winCon;
+        [SerializeField]
+        private AudioClip _loseCon;
 
         private UnityAction _onButtonClick;
         private UnityAction _onEnemyDie;
         private UnityAction _onBuyTower;
         private UnityAction _onSellTower;
+        private UnityAction _onPlantAttack1;
+        private UnityAction _onPlantAttack2;
+        private UnityAction _onPlantAttack3;
+        private UnityAction _onPlantAttack4;
+        private UnityAction _onUpgradeTower;
+        private UnityAction _onBoss;
+        private UnityAction _onWin;
+        private UnityAction _onLose;
 
         private void Awake()
         {
@@ -44,6 +68,14 @@ namespace Plantastic.Module_GameAudio
             _onEnemyDie = new UnityAction(OnEnemyDie);
             _onBuyTower = new UnityAction(OnBuyTower);
             _onSellTower = new UnityAction(OnSellTower);
+            _onPlantAttack1 = new UnityAction(OnPlantAttack1);
+            _onPlantAttack2 = new UnityAction(OnPlantAttack2);
+            _onPlantAttack3 = new UnityAction(OnPlantAttack3);
+            _onPlantAttack4 = new UnityAction(OnPlantAttack4);
+            _onUpgradeTower = new UnityAction(OnUpgradeTower);
+            _onBoss = new UnityAction(OnBoss);
+            _onWin = new UnityAction(OnWin);
+            _onLose = new UnityAction(OnLose);
         }
         private void OnEnable()
         {
@@ -51,6 +83,14 @@ namespace Plantastic.Module_GameAudio
             EventManager.StartListening("DieMessage", _onEnemyDie);
             EventManager.StartListening("BuyMessage", _onBuyTower);
             EventManager.StartListening("SellMessage", _onSellTower);
+            EventManager.StartListening("Attack1Message", _onPlantAttack1);
+            EventManager.StartListening("Attack2Message", _onPlantAttack2);
+            EventManager.StartListening("Attack3Message", _onPlantAttack3);
+            EventManager.StartListening("Attack4Message", _onPlantAttack4);
+            EventManager.StartListening("UpgradeMessage", _onUpgradeTower);
+            EventManager.StartListening("BossMessage", _onBoss);
+            EventManager.StartListening("WinSFXMessage", _onWin);
+            EventManager.StartListening("LoseSFXMessage", _onLose);
         }
         private void OnDisable()
         {
@@ -58,6 +98,15 @@ namespace Plantastic.Module_GameAudio
             EventManager.StopListening("DieMessage", _onEnemyDie);
             EventManager.StopListening("BuyMessage", _onBuyTower);
             EventManager.StopListening("SellMessage", _onSellTower);
+            EventManager.StopListening("Attack1Message", _onPlantAttack1);
+            EventManager.StopListening("Attack2Message", _onPlantAttack2);
+            EventManager.StopListening("Attack3Message", _onPlantAttack3);
+            EventManager.StopListening("Attack4Message", _onPlantAttack4);
+            EventManager.StopListening("UpgradeMessage", _onUpgradeTower);
+            EventManager.StopListening("BossMessage", _onBoss);
+            EventManager.StopListening("WinSFXMessage", _onWin);
+            EventManager.StopListening("LoseSFXMessage", _onLose);
+
         }
         private void Update()
         {
@@ -82,6 +131,38 @@ namespace Plantastic.Module_GameAudio
         public void OnSellTower()
         {
             PlaySfx(_sellTower);
+        }
+        public void OnPlantAttack1()
+        {
+            PlaySfx(_plantAttack1);
+        }
+        public void OnPlantAttack2()
+        {
+            PlaySfx(_plantAttack2);
+        }
+        public void OnPlantAttack3()
+        {
+            PlaySfx(_plantAttack3);
+        }
+        public void OnPlantAttack4()
+        {
+            PlaySfx(_plantAttack4);
+        }
+        public void OnUpgradeTower()
+        {
+            PlaySfx(_upgradeTower);
+        }
+        public void OnBoss()
+        {
+            PlaySfx(_boss);
+        }
+        public void OnWin()
+        {
+            PlaySfx(_winCon);
+        }
+        public void OnLose()
+        {
+            PlaySfx(_loseCon);
         }
     }
 }
